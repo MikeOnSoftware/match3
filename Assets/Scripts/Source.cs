da0dabf0414457adb1f7459ef971bf3b89111edf
Mihail Rusanov <rusanov.mihail@gmail.com>
2024-04-12 14:08:40 +0300

    first commit

---
+++ Assets/Scripts/Source.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Source : MonoBehaviour
+{
+    [SerializeField] internal int    width;
+    [SerializeField] internal int    height;
+    [SerializeField] GameObject[]    tilePrefabs;
+    [SerializeField] GameObject[]    hiders;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] GameObject      restartButton;
+
+    GameObject[,] tiles;
+
+    internal GameObject       clickedObject;
+    internal List<GameObject> clickedObjects;
+
+    int score = 0;
+
+    void Start()
+    {
+        tiles = new GameObject[width, height];
+        SetupGameBoard();
+        clickedObjects = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        CheckForMatch();
+    }
+
+    void SetupGameBoard()
+    {
+        float tileWidth = tilePrefabs[0].GetComponent<RectTransform>().sizeDelta.x;
+        float tileHeight = tilePrefabs[0].GetComponent<RectTransform>().sizeDelta.y;
+        float xOffset = tileWidth;
+        float yOffset = tileHeight;
+
+        for (int x = 0; x < width; x++)
+        {
+            xOffset *= (2 + x);
+            for (int y = 0; y < height; y++)
+            {
+                yOffset *= (2 + y);
+
+                Vector3 position = new Vector3(x + xOffset, y + yOffset, 0);
+                GameObject tile = Instantiate(GetRandomTilePrefab(), position, Quaternion.identity) as GameObject;
+                tile.transform.parent = this.transform;
+                tiles[x, y] = tile;
+
+                yOffset = tileHeight;
+            }
+            xOffset = tileWidth;
+        }
+        foreach (var tile in tilePrefabs)
+        {
+            Destroy(tile);
+        }
+    }
+
+    GameObject GetRandomTilePrefab()
+    {
+        return tilePrefabs[Random.Range(0, tilePrefabs.Length)];
+    }
+
+    void CheckForMatch()
+    {
+        if (clickedObjects.Count == 2)
+        {
+            if (clickedObjects[0].name == clickedObjects[1].name)
+            {
+                score++;
+                Debug.Log(score);
+                scoreText.text = "Score: " + score.ToString();
+            }
+            Destroy(clickedObjects[0], .2f);
+            Destroy(clickedObjects[1], .2f);
+            clickedObjects = new List<GameObject>();
+        }
+    }
+
+    public void OnRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
