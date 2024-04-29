75cc95d85d444517a0eb7f8cfd5db21439948a94
Mihail Rusanov <rusanov.mihail@gmail.com>
2024-04-29 18:27:51 +0300

    Refactor GameBoard.cs

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
-    [SerializeField] internal int row;
-    [SerializeField] internal int col;
+    [SerializeField] int row;
+    [SerializeField] int col;
+
     [SerializeField] GameObject[] tilePrefabs;
     [SerializeField] GameObject[] hiders;
 
@@ -20,33 +21,20 @@ public class GameBoard : MonoBehaviour
 
     void SetupGameBoard()
     {
-        float tileWidth = tilePrefabs[0].GetComponent<RectTransform>().sizeDelta.x;
-        float tileHeight = tilePrefabs[0].GetComponent<RectTransform>().sizeDelta.y;
-        float xOffset = tileWidth;
-        float yOffset = tileHeight;
-
         for (int x = 0; x < row; x++)
         {
-            xOffset *= (2 + x);
             for (int y = 0; y < col; y++)
             {
-                yOffset *= (2 + y);
-
-                Vector3 position = new Vector3(x + xOffset, y + yOffset, 0);
+                Vector3 position = new Vector3(x , y , 0);
                 GameObject tile = Instantiate(GetRandomTilePrefab(), position, Quaternion.identity) as GameObject;
                 tile.transform.parent = this.transform;
                 tiles[x, y] = tile;
-
-                yOffset = tileHeight;
             }
-            xOffset = tileWidth;
         }
         foreach (var tile in tilePrefabs)
         {
             Destroy(tile);
         }
     }
-
     GameObject GetRandomTilePrefab() => tilePrefabs[Random.Range(0, tilePrefabs.Length)];
-
 }
